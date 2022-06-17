b4d00c6f3fde64b00db626ef409b12870bda3627
krishn64 <72079144+krishn64@users.noreply.github.com>
2022-06-17 12:52:56 +0530

    Add files via upload

---
+++ call.py
@@ -0,0 +1,23 @@
+from traceback import print_tb
+
+
+class Videogames:
+    def __init__(self,color,name):
+        self.color=color
+        self.name=name
+    
+    
+    def playgames(self):
+        print("Don`t study(other than coding)")
+    
+    
+games=Videogames("blue","fun")
+games.playgames()
+
+        
+        
+    
+        
+
+
+        
